e2fa9bc4ebcf1f8f4bd56f8d41f018d19c637630
Patrick Her <renfu.her@gmail.com>
2024-09-23 15:52:53 +0800

    `Refactor web routes to use a catch-all route for non-API requests`

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/{any?}', 'app')->where('any', '^(?!api\/)[\/\w\.-]*');
+Route::match(['get', 'post'], '/', function () {
+    return '';
+});
