f5b8addf0348bff7688d4e4761f06551fb6b462a
Patrick Her <renfu.her@gmail.com>
2024-09-23 20:20:12 +0800

    `Refactor MailController to include 'from' field in email sending`

--- app/Http/Controllers/API/MailController.php
+++ app/Http/Controllers/API/MailController.php
@@ -20,7 +20,7 @@ class MailController extends Controller
     {
         // 验证请求数据
         $request->validate([
-            'app_name' => 'required|string',
+            'from' => 'required|array',
             'emails' => 'required|array', // 接受多个 email
             'emails.*' => 'required|email', // 每个 email 必须是有效的
             'subject' => 'required|string',
@@ -36,9 +36,9 @@ class MailController extends Controller
         // 动态设置邮件用户名和密码
         Config::set('mail.username', $request->input('mail_username'));
         Config::set('mail.password', $request->input('mail_password'));
-        Config::set('app.name', $request->input('app_name'));
         $emails = $request->input('emails');
         $subject = $request->input('subject');
+        $from = $request->input('from');
         $messageContent = $request->input('message');
         $cc = $request->input('cc', []); // 获取 cc 字段，默认为空数组
         $bcc = $request->input('bcc', []); // 获取 bcc 字段，默认为空数组
@@ -57,7 +57,7 @@ class MailController extends Controller
                 $mail->bcc($bcc);
             }
 
-            $mail->send(new CustomMail($messageContent, $subject));
+            $mail->send(new CustomMail($messageContent, $subject, $from));
         }
 
         // 返回成功响应
--- app/Mail/CustomMail.php
+++ app/Mail/CustomMail.php
@@ -12,17 +12,19 @@ class CustomMail extends Mailable
 
     public $messageContent;
     public $subjectText;
-
+    public $from;
     /**
      * Create a new message instance.
      *
      * @param string $messageContent
      * @param string $subjectText
+     * @param array $from
      */
-    public function __construct($messageContent, $subjectText)
+    public function __construct($messageContent, $subjectText, $from)
     {
         $this->messageContent = $messageContent;
         $this->subjectText = $subjectText;
+        $this->from = $from;
     }
 
     /**
@@ -33,6 +35,7 @@ class CustomMail extends Mailable
     public function build()
     {
         return $this->subject($this->subjectText)
+            ->from($this->from)
             ->html($this->messageContent) // 使用 HTML 內容來渲染郵件
             ->view('emails.custom_mail') // 使用 Blade 模板來渲染郵件
             ->with([
